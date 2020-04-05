3e7f8fd1c58e45fa69ce6a20a5afada8925e9d5a
Richard Kasperowski <r@kasperowski.com>
2020-04-04 22:52:17 -0400

    Find the roman input parameter

--- index.py
+++ index.py
@@ -6,7 +6,8 @@ def handler(event, context):
     data = {
         'output': 'Hello Richard',
         'timestamp': datetime.datetime.utcnow().isoformat(),
-        'event' : event
+        'event' : event,
+        'roman' : event['queryStringParameters']['roman']
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,7 +6,11 @@ class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler(None, None)
+        event = {}
+        param = {}
+        param['roman'] = 'V'
+        event['queryStringParameters'] = param
+        result = index.handler(event, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
