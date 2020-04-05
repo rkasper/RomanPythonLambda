218aae2b3f1260d310ade61a3acbaea3e14850fe
Richard Kasperowski <r@kasperowski.com>
2020-04-04 22:17:18 -0400

    Add input event to the output

--- index.py
+++ index.py
@@ -5,7 +5,8 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello Richard',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'event' : event
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
