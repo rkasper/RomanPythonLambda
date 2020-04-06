5d032b0b9c3abaf98666e7077b6ad9b12682cf03
Richard Kasperowski <r@kasperowski.com>
2020-04-06 17:57:07 -0400

    Oh gosh - I had forgotten the unit test for L!

--- tests/test_roman.py
+++ tests/test_roman.py
@@ -9,6 +9,7 @@ class TestRomanCase(unittest.TestCase):
         self.assertEqual(1, convert("I"))
         self.assertEqual(5, convert("V"))
         self.assertEqual(10, convert("X"))
+        self.assertEqual(50, convert("L"))
 
 
 if __name__ == '__main__':
